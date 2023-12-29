f142359a3c85317992894f3c2783ed5e1a4d155e
behong1999 <khonhong46632@gmail.com>
2023-12-29 21:57:43 +0200

    create pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
 
 type Props = {}
 
 const App = (props: Props) => {
   return (
-    <h2>App</h2>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      
+      </Routes></BrowserRouter>
   )
 }
