91c425b6ac83941a03963b6273e9c0b6b477e85a
behong1999 <khonhong46632@gmail.com>
2024-01-08 18:59:47 +0200

    sign up and error handler for the server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,6 +16,21 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    statusCode,
+    message,
+  });
 });
