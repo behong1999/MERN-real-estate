5f8d19820324301c3d288cff5e830b307d3d353b
behong1999 <khonhong46632@gmail.com>
2024-01-31 11:47:47 +0200

    Sign In without showing password

--- api/controllers/auth.controller.ts
+++ api/controllers/auth.controller.ts
@@ -1,11 +1,14 @@
 import User from '../models/user.model';
 import { NextFunction, Request, Response } from 'express';
 import bcrypt from 'bcrypt';
+import { errorHandler } from '../utils/error';
+import jwt from 'jsonwebtoken';
 
-// import { errorHandler } from '../utils/error.js';
-// import jwt from 'jsonwebtoken';
-
-export const signUp = async (req:Request, res:Response, next:NextFunction) => {
+export const signUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -17,23 +20,31 @@ export const signUp = async (req:Request, res:Response, next:NextFunction) => {
   }
 };
 
-// export const signIn = async (req, res, next) => {
-//   const { email, password } = req.body;
-//   try {
-//     const validUser = await User.findOne({ email });
-//     if (!validUser) return next(errorHandler(404, 'User Not Found!'));
-//     const validPassword = bcryptjs.compareSync(password, validUser.password);
-//     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
-//     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-//     const { password: pass, ...rest } = validUser._doc;
-//     res
-//       .cookie('access_token', token, { httpOnly: true })
-//       .status(200)
-//       .json(rest);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+export const signIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User Not Found!'));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    const token = jwt.sign(
+      { id: validUser._id },
+      process.env.JWT_SECRET as string
+    );
+    const { password: pass, ...rest } = validUser.toObject();
+    // Avoid cross-site scripting attacks
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // export const google = async (req, res, next) => {
 //   try {
